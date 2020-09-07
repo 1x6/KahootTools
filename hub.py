e6251958e7375f21282fee2181a8a544da0249b5
Tic <44981148+yougotsyskeyed@users.noreply.github.com>
2020-09-07 13:34:58 +0100

    Add files via upload

---
+++ hub.py
@@ -0,0 +1,7 @@
+import spamjoin
+
+print('KahootTools by Alec.')
+ask1 = input('Would you like to run the spammer? y/n ')
+if ask1 == 'y':
+    spamjoin.spamjoin()
+
---
+++ spamjoin.py
@@ -0,0 +1,32 @@
+from kahoot import client
+import string
+import random
+import time
+
+
+
+bot = client()
+
+
+def spamjoin() :
+    gameid = input('Enter the gameid: ')
+    botamount = input('Enter the amount of bots (max 2000): ')
+    custom_usr = input('Enter the desired username for your bots: ')
+
+    def joingame() :
+        #yep = gen_user()
+        #bot.join((gameid), f"{yep}")
+
+        usrname = (custom_usr + str(random.randint(1, 100000)))
+
+        bot.join((gameid), usrname)
+
+        def joinHandle() :
+            pass
+
+        bot.on("joined", joinHandle)
+        print(f"Joined game {gameid} with username {usrname}.")
+        # time.sleep(.1)
+
+    for x in range(0, (int(botamount) )) : # + 10  this is not code
+        joingame()
